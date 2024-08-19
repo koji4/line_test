c7bad416973378ae3168bbe88f00f82a355bbda4
yakiutokyo <yakiu.tokyo@gmail.com>
2024-08-19 10:24:41 +0900

    neko2

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,22 @@
 import { useEffect } from 'react';
-import liff from '@line/liff';
-import '@/styles/globals.css';
 
-export default function App({ Component, pageProps }) {
+const LIFF_ID = '2006108606-o0KqQrjk'; // LINE Developersで取得したLIFF IDに置き換えてください
+
+function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    // LIFF SDKの初期化
-    liff.init({ liffId: '2006104869-3VgkYXeG' }).then(() => {
-      if (!liff.isLoggedIn()) {
-        liff.login();
+    (async () => {
+      try {
+        await window.liff.init({ liffId: LIFF_ID });
+        if (!window.liff.isLoggedIn()) {
+          window.liff.login();
+        }
+      } catch (error) {
+        console.error('LIFF Initialization failed ', error);
       }
-    }).catch(err => {
-      console.error('LIFF initialization failed', err);
-    });
+    })();
   }, []);
 
   return <Component {...pageProps} />;
 }
+
+export default MyApp;
